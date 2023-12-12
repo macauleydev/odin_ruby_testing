673b3de3a329e10052ad70b757658210ef0e86d2
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-12-12 09:10:16 -0500

    Complete lesson 11 (11b)

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,19 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
+  subject(:zoey) { Cat.new("Zoey", "tuxedo", "black and white") }
 
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  include_examples 'shared method name'
+
+  # remove the 'x' before running this test
+  it 'is not hungry' do
+    expect(zoey).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
-  end
-
-  # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(zoey).to be_hiding
   end
 end
